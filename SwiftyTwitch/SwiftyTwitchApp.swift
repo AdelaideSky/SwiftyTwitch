684b2cfa2ddf50214fd5d7fcf48e7b553304cc87
Adélaïde Sky <augustin.humez@gmail.com>
2022-12-26 15:02:06 +0100

    Initial Commit

---
+++ SwiftyTwitch/SwiftyTwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyTwitchApp.swift
+//  SwiftyTwitch
+//
+//  Created by Adélaïde Sky on 26/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyTwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
